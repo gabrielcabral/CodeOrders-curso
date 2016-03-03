90aea18821aa807fb856b5c6cb2d3f6ad5fa99be
gabrielcabral <cabraldealmeida@gmail.com>
2016-03-03 10:43:12 -0300

    alteração

---
+++ module/CodeOrders/src/CodeOrders/V1/Rest/Orders/OrderItemTableGatewayFactory.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: GABRIEL
+ * Date: 29/02/2016
+ * Time: 14:27
+ */
+
+namespace CodeOrders\V1\Rest\Orders;
+
+
+use Zend\Db\TableGateway\TableGateway;
+use Zend\ServiceManager\FactoryInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+
+class OrderItemTableGatewayFactory implements FactoryInterface
+{
+
+    /**
+     * Create service
+     *
+     * @param ServiceLocatorInterface $serviceLocator
+     * @return mixed
+     */
+    public function createService(ServiceLocatorInterface $serviceLocator)
+    {
+        $dbAdapter = $serviceLocator->get('DbAdapter');
+
+        $hydra = new HydratingResultSet(new ClassMethods(), new OrderItemEntity());
+
+        $tableGateway = new TableGateway('order_item',$dbAdapter, null ,$hydra);
+
+        return $tableGateway;
+    }
+}
